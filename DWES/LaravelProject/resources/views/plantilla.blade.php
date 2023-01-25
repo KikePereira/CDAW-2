e7665416d1fef250dc4b2629ba21bb9d185bf807
Kike <kikepereiraramospr@gmail.com>
2023-01-25 09:40:45 +0100

    logo

--- DWES/LaravelProject/resources/views/plantilla.blade.php
+++ DWES/LaravelProject/resources/views/plantilla.blade.php
@@ -28,7 +28,10 @@
         <!-- Sidebar -->
         <div id="sidebar-container" class="bg-primary">
             <div class="logo">
-                <h4 class="text-light font-weight-bold mb-0">LOGO</h4>
+                <h4 class="text-light font-weight-bold mb-0">
+                    LOGO
+                </h4>
+                <img src="logo.png" alt="">
             </div>
             <div class="menu">
                 <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i>
